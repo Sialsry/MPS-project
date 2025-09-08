236868f58dacc2b440696eb641fd65eabcc0becd
Sialsry <alsryendtm@naver.com>
2025-09-08 19:09:17 +0900

    [fix] 불필요한 파일 정리

--- drizzle/0000_icy_gladiator.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"age" integer,
-	"created_at" timestamp DEFAULT now()
-);
--- src/db.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import postgres from 'postgres';
-import { drizzle } from 'drizzle-orm/postgres-js';
-
-const client = postgres(process.env.DATABASE_URL!, {
-    max: 1, // 간단한 앱이면 1로도 충분
-});
-export const db = drizzle(client);
